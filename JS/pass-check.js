8f595ba798f405bdccb8fdd5c36222c4720c6a52
Alexey Hramenkov <131589019+scramble22@users.noreply.github.com>
2024-01-23 16:40:53 +0300

    v2.1

--- JS/pass-check.js
+++ JS/pass-check.js
@@ -1,9 +1,26 @@
+// check-auth.js
 window.onload = function () {
-    const correctPassword = "жопа хуй 777";
-    const storedPassword = localStorage.getItem("pass");
-
-    if (storedPassword !== correctPassword) {
-        // Перенаправление на страницу входа, если пароль не совпадает
+    const storedUsername = localStorage.getItem("username");
+    
+    if (!storedUsername || !isUserActive(storedUsername)) {
+        // Перенаправление на страницу входа, если пользователь не авторизован или истек срок действия
         window.location.href = "login.html";
     }
+
+    // Отображение имени пользователя на странице
+    document.getElementById("username").innerText = storedUsername;
 };
+
+function isUserActive(username) {
+    // Поиск пользователя с введенным именем
+    const user = users.find(u => u.username === username);
+
+    // Проверка, активен ли пользователь на текущий момент
+    if (user) {
+        const currentDate = new Date();
+        const expirationDate = new Date(user.expirationDate);
+        return currentDate <= expirationDate;
+    }
+
+    return false;
+}
---
+++ users.js
@@ -0,0 +1,6 @@
+// Создаем массив пользователей
+const users = [
+    { username: "admin", password: "sexsigma228", expirationDate: "2024-01-31" },
+    { username: "user2", password: "password2", expirationDate: "2024-01-31" },
+    // Добавь больше пользователей по необходимости
+];
